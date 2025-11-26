88de7ea131150f25fc03cbef40fab92a808d47f4
Dahlia Ghonim <dahlia.ghonim@gmail.com>
2025-11-26 19:56:38 +0200

    Generate Board scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_24_174728) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_26_175141) do
+  create_table "boards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -22,4 +30,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_24_174728) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "boards", "users"
 end
