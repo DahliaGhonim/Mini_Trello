268fb6223131cc92a72ccc7218d0f6b574735d9a
Dahlia Ghonim <dahlia.ghonim@gmail.com>
2025-12-04 05:41:11 +0200

    Implement manual move of a card across lists, boards, and inbox

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,6 @@
 class BoardsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_board, only: %i[ show edit update destroy ]
+  before_action :set_board, only: %i[ show edit update destroy lists_json ]
 
   def index
     @boards = current_user.boards
@@ -18,32 +18,50 @@ class BoardsController < ApplicationController
 
   def create
     @board = current_user.boards.new(board_params)
-    @board.save
 
     respond_to do |format|
-      format.turbo_stream
+      if @board.save
+        format.turbo_stream
+        format.json { render :show, status: :created, location: @board }
+      else
+        format.turbo_stream
+        format.json { render json: @board.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
-    if @board.update(board_params)
-      redirect_to @board, notice: "Board was successfully updated.", status: :see_other
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @board.update(board_params)
+        format.html { redirect_to @board, notice: "Board was successfully updated." }
+        format.json { render :show, status: :ok, location: @board }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @board.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @board.destroy!
-    redirect_to boards_path, notice: "Board was successfully destroyed.", status: :see_other
+
+    respond_to do |format|
+      format.html { redirect_to boards_path, notice: "Board was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  def lists_json
+    lists = @board.lists.select(:id, :name)
+    render json: lists
   end
 
   private
-    def set_board
+  def set_board
       @board = current_user.boards.find(params.expect(:id))
-    end
+  end
 
-    def board_params
+  def board_params
       params.expect(board: [ :user_id, :name ])
-    end
+  end
 end
